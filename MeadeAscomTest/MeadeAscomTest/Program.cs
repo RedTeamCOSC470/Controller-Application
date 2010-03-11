395a6686ee249dbd20cc328677f023878fb11b18
unknown <ocstudent@.(none)>
2010-03-11 15:21:13 -0800

    Updated program code to make it more robust

--- MeadeAscomTest/MeadeAscomTest/Program.cs
+++ MeadeAscomTest/MeadeAscomTest/Program.cs
@@ -15,19 +15,22 @@ namespace MeadeAscomTest
              * need to be converted to fractions of an hour.
              * 20.00 = RA of 20:00:00
              * 21.5 = RA of 21:30:00
-             * 30/60 = .5
-             * 60*60 = 3600
              * hours + minutes/60 + seconds/3600
              */
-            double MinutesInHour = 60;   //avoiding magiv numbers... might be needed
+            double MinutesInHour = 60;   //avoiding magic numbers... might be needed
             double SecondsInHour = 3600; //if you are observing on Mars...?
             return (Hour + (Minutre / MinutesInHour) + (Second / SecondsInHour));
         }
 
-        static double TimeToRA(DateTime time)
+        static double timeToRA(DateTime Time)
         {
-            return getRA(time.Hour, time.Minute, time.Second);
+            /*
+             * Gets the hour minute and second of a date time variable 
+             * and passes those values to the getRA method
+             */
+            return getRA(Time.Hour, Time.Minute, Time.Second);
         }
+
         static void Main(string[] args)
         {
             /*
@@ -41,8 +44,11 @@ namespace MeadeAscomTest
             MeadeEx.TelescopeClass myScope = new MeadeEx.TelescopeClass();
             try
             {
+                myScope.Connected = true; // connect to the telescope
 
-                myScope.Connected = true;
+                double RightAscension = -1; // variables for later
+                double Declination;
+                DateTime RA;
 
                 if (myScope.AtPark)
                 {
@@ -53,39 +59,58 @@ namespace MeadeAscomTest
                     myScope.Tracking = true;
                 }
 
-                double RightAscension = getRA(16, 45, 12);
-                double Declination = 23.00;
+                // Set up done, checking inputs to find out what we want to do.
 
-                if (args.Length == 2)
+                if (args.Length == 0)
                 {
-                    DateTime RA;
-                    DateTime.TryParse(args[0], out RA);
+                    //testing mode do something smart here
+                    Console.Out.WriteLine("Please use the program by providing the coordinates to point the telescope to or the " + 
+                        "the name of the object from the MiniSAC catalog.");
+                }
+                else if (args.Length == 2 && DateTime.TryParse(args[0], out RA) && double.TryParse(args[1], out Declination))
+                {
+                    // normal coordinates entered
+                    Console.Out.WriteLine("Normal Mode");
+                    
                     if (RA != DateTime.MinValue)
                     {
-                        RightAscension = TimeToRA(RA);
-                        double.TryParse(args[1], out Declination);
+                        RightAscension = timeToRA(RA);
                     }
                     else
                     {
-                        Console.Error.WriteLine("Could not convert the value {0} to a time." ,args[0]);
+                        Console.Error.WriteLine("Could not convert the value {0} to a time.", args[0]);
                     }
                     myScope.SlewToCoordinates(RightAscension, Declination);
                 }
-                else if (args.Length == 1)
+                else
                 {
-                    if (args[0] == "PARK" && myScope.CanPark)
+                    // location name entered - one or more arg values combine them into 1 arg
+                    Console.Out.WriteLine("Special Mode");
+                    string theArg = "";
+                    for (int n = 0; n < args.Length; n++)
                     {
-                        if (myScope.CanPark)
+                        if (string.Compare(theArg, "") != 0)
+                            theArg += " ";
+                        theArg += args[n];
+                    }
+                    Console.Out.WriteLine("Got Arg: '" + theArg + "'");
+                    //Console.Out.WriteLine("==PARK?: " + (string.Compare(theArg, "PARK") == 0));
+                    //Console.Out.WriteLine("==park?: " + (string.Compare(theArg, "park") == 0));
+                    //Console.Out.WriteLine("==Park: " + (string.Compare(theArg, "Park") == 0));
+                    //Console.Out.WriteLine("can park?: " + myScope.CanPark);
+                    //Console.Out.WriteLine("So then we can park?: " + (string.Compare(theArg, "PARK") == 0 || string.Compare(theArg, "park") == 0 || string.Compare(theArg, "Park") == 0));
+                    if ((string.Compare(theArg, "PARK") == 0 || string.Compare(theArg, "park") == 0 || string.Compare(theArg, "Park") == 0)
+                        && myScope.CanPark)
+                    {
+                        Console.Out.WriteLine("Going to Park");
+                        if (myScope.CanSetPark)
                         {
                             myScope.SetPark();
-                            myScope.Park();
                         }
-                        else
-                        {
-                            Console.Error.WriteLine("You can not park this type of telescope.");
-                        }
+                        myScope.Park();
                     }
-                    if (args[0] == "UNPARK" && myScope.AtPark)
+                        else if ((string.Compare(theArg, "UNPARK") == 0 || string.Compare(theArg, "unpark") == 0 || string.Compare(theArg, "Unpark") == 0) 
+                        && myScope.AtPark)
                     {
                         if (myScope.CanUnpark)
                         {
@@ -95,6 +120,19 @@ namespace MeadeAscomTest
                         {
                             Console.Error.WriteLine("You can not unpark this type of telescope automatically.");
                         }
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("Going To Move");
+
+                        MiniSAC.CatalogClass miniCatalog = new MiniSAC.CatalogClass();
+                        miniCatalog.SelectObject(theArg);
+                            float tempRA = miniCatalog.RightAscension;
+                            float tempDec = miniCatalog.Declination;
+                         Console.Out.WriteLine("Moving to: {0}, {1}", tempRA, tempDec);
+                        Console.Out.WriteLine(miniCatalog.RightAscension);
+                        Console.Out.WriteLine(miniCatalog.Declination);
+                        myScope.SlewToCoordinates(tempRA, tempDec);
                     }
                 }
             }
---
+++ MeadeAscomTest/MeadeAscomTest/TestProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MeadeAscomTest
+{
+    [TestFixture]
+    class TestProgram
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
